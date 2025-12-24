35cc3bfc1c562b3819096067d0f8bfe04625316e
leexiaohui1997 <1287338537@qq.com>
2025-12-24 11:32:30 +0800

    feat(web): 引入 element-plus 组件库

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,25 @@
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath } from 'node:url';
+import AutoImport from 'unplugin-auto-import/vite';
+import ElementPlus from 'unplugin-element-plus/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    ElementPlus({
+      useSource: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
